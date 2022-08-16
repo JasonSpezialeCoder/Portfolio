3762d553845b12cb81e65bd0c0cd3b06e2dbe54a
Jason Speziale <python5632@gmail.com>
2022-08-16 16:17:38 -0400

    initial commit

---
+++ html&css/assets/js/main.js
@@ -0,0 +1,31 @@
+// splide js 
+
+var splide = new Splide( '.splide', {
+  type   : 'loop',
+  perPage: 3,
+  perMove: 1,
+  arrows: true,
+  paginationDirection: false,
+  pagination: false,
+  gap: 30,
+  
+  breakpoints: {
+    600: {
+			perPage: 1,
+		},
+    1000: {
+			perPage: 2,
+		},
+		900: {
+			perPage: 2,
+		},
+  },
+} );
+
+
+splide.mount();
+
+// form 
+
+var redirect = document.getElementById('redirect');
+redirect.value = new URL('success.html', location).href;
